335a15944dc4f85ce299ff8fa0174766c9786090
Ratte222 <Artur21111999.o@gmail.com>
2021-09-24 17:17:10 +0300

    made an example from the microsoft ILoggerProvider

---
+++ NoNameLogger.AspNetCore/ColorConsoleLoggerProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NoNameLogger.AspNetCore.Config;
+
+namespace NoNameLogger.AspNetCore
+{
+    public sealed class ColorConsoleLoggerProvider : ILoggerProvider
+    {
+        private readonly IDisposable _onChangeToken;
+        private ColorConsoleLoggerConfiguration _currentConfig;
+        private readonly ConcurrentDictionary<string, NoNameLogger> _loggers = new ConcurrentDictionary<string, NoNameLogger>();
+
+        public ColorConsoleLoggerProvider(
+            IOptionsMonitor<ColorConsoleLoggerConfiguration> config)
+        {
+            _currentConfig = config.CurrentValue;
+            _onChangeToken = config.OnChange(updatedConfig => _currentConfig = updatedConfig);
+        }
+
+        public ILogger CreateLogger(string categoryName) =>
+            _loggers.GetOrAdd(categoryName, name => new NoNameLogger(name, GetCurrentConfig));
+
+        private ColorConsoleLoggerConfiguration GetCurrentConfig() => _currentConfig;
+
+        public void Dispose()
+        {
+            _loggers.Clear();
+            _onChangeToken.Dispose();
+        }
+    }
+}
---
+++ NoNameLogger.AspNetCore/Config/ColorConsoleLoggerConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoNameLogger.AspNetCore.Config
+{
+    public class ColorConsoleLoggerConfiguration
+    {
+        public int EventId { get; set; }
+
+        public Dictionary<LogLevel, ConsoleColor> LogLevels { get; set; } = new Dictionary<LogLevel, ConsoleColor>()
+        {
+            [LogLevel.Information] = ConsoleColor.Green
+        };
+    }
+}
